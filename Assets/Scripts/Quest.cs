26596d84be42ae1bacbdf3255d7e9fdb9fd6c3a0
Poh <nicholasrandomz@yahoo.com.sg>
2018-01-21 12:35:42 +0800

    Resized Community, Added Quest, CharacterCreation Scene
    TODO -
    Create Character Scene Options
    Quest Repeatables/Events examples - maybe a legit quest c# script
    Party Page(?)

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quest : MonoBehaviour {
+    [SerializeField]
+    GameObject quest;
+
+    // Use this for initialization
+    void Start()
+    {
+        quest.GetComponent<Text>().text = "Save the village \n" +
+            "\t\t\t Kill 100 Goblins " +
+            "\t\t\t\t\t\t\t\t\t\t\t  Exp 200 Gold 500";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnQuestClick()
+    {
+        quest.GetComponent<Text>().text = "Save the village \n" +
+            "\t\t\t Find Anna " +
+            "\t\t\t\t\t\t\t\t\t\t\t\t\t Exp 50 Gold 100";
+    }
+}
---
+++ Assets/Scripts/SceneTransition/SelectCharacterSceneTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectCharacterSceneTransition : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //SceneManager.LoadScene("Game");
+    public void ToBack()
+    {
+        SceneManager.LoadScene("AccountInfo");
+    }
+    public void ToCreate() { Debug.Log("To Create"); }
+    public void ToDelete() { Debug.Log("To Delete"); }
+    public void ToPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
