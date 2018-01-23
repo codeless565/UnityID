eb0efe686746c89dd6e4ca190c7b31f7f9016dbf
Poh <nicholasrandomz@yahoo.com.sg>
2018-01-24 00:46:45 +0800

    :D

---
+++ Assets/Scripts/CreateCharacter.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateCharacter : MonoBehaviour {
+
+    [SerializeField]
+    GameObject ConfirmedGender;
+    [SerializeField]
+    GameObject ConfirmedNose;
+    [SerializeField]
+    GameObject ConfirmedEyes;
+    [SerializeField]
+    GameObject ConfirmedMouth;
+    
+    string ConfirmedName;
+
+    // Gender
+    [SerializeField]
+    GameObject GenderOne;
+    [SerializeField]
+    GameObject GenderTwo;
+
+    // Nose
+    [SerializeField]
+    GameObject NoseOne;
+    [SerializeField]
+    GameObject NoseTwo;
+    [SerializeField]
+    GameObject NoseThree;
+    [SerializeField]
+    GameObject NoseFour;
+
+    // Eyes
+    [SerializeField]
+    GameObject EyesOne;
+    [SerializeField]
+    GameObject EyesTwo;
+    [SerializeField]
+    GameObject EyesThree;
+    [SerializeField]
+    GameObject EyesFour;
+
+    // Mouths
+    [SerializeField]
+    GameObject MouthOne;
+    [SerializeField]
+    GameObject MouthTwo;
+    [SerializeField]
+    GameObject MouthThree;
+    [SerializeField]
+    GameObject MouthFour;
+
+    [SerializeField]
+    GameObject Name;
+
+    bool selectedGender;
+   bool selectedNose;
+   bool selectedEyes;
+   bool selectedMouth;
+
+    // Use this for initialization
+    void Start () {
+        selectedGender = false;
+        selectedNose = false;
+        selectedEyes = false;
+        selectedMouth = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ConfirmedGender.SetActive(selectedGender);
+        ConfirmedNose.SetActive(selectedNose);
+        ConfirmedEyes.SetActive(selectedEyes);
+        ConfirmedMouth.SetActive(selectedMouth);
+    }
+
+    public void NameUpdate()
+    {
+        ConfirmedName = Name.GetComponent<InputField>().text;
+    }
+
+    // Gender
+    public void SelectedGender1()
+    {
+        selectedGender = true;
+        ConfirmedGender.GetComponent<Image>().sprite = GenderOne.GetComponent<Image>().sprite;
+    }
+    public void SelectedGender2()
+    {
+        selectedGender = true;
+        ConfirmedGender.GetComponent<Image>().sprite = GenderTwo.GetComponent<Image>().sprite;
+    }
+
+    // Nose
+    public void SelectedNose1()
+    {
+        selectedNose = true;
+        ConfirmedNose.GetComponent<Image>().sprite = NoseOne.GetComponent<Image>().sprite;
+    }
+    public void SelectedNose2()
+    {
+        selectedNose = true;
+        ConfirmedNose.GetComponent<Image>().sprite = NoseTwo.GetComponent<Image>().sprite;
+    }
+    public void SelectedNose3()
+    {
+        selectedNose = true;
+        ConfirmedNose.GetComponent<Image>().sprite = NoseThree.GetComponent<Image>().sprite;
+    }
+    public void SelectedNose4()
+    {
+        selectedNose = true;
+        ConfirmedNose.GetComponent<Image>().sprite = NoseFour.GetComponent<Image>().sprite;
+    }
+
+    // Eyes
+    public void SelectedEyes1()
+    {
+        selectedEyes = true;
+        ConfirmedEyes.GetComponent<Image>().sprite = EyesOne.GetComponent<Image>().sprite;
+    }
+    public void SelectedEyes2()
+    {
+        selectedEyes = true;
+        ConfirmedEyes.GetComponent<Image>().sprite = EyesTwo.GetComponent<Image>().sprite;
+    }
+    public void SelectedEyes3()
+    {
+        selectedEyes = true;
+        ConfirmedEyes.GetComponent<Image>().sprite = EyesThree.GetComponent<Image>().sprite;
+    }
+    public void SelectedEyes4()
+    {
+        selectedEyes = true;
+        ConfirmedEyes.GetComponent<Image>().sprite = EyesFour.GetComponent<Image>().sprite;
+    }
+
+    // Mouth
+    public void SelectedMouth1()
+    {
+        selectedMouth = true;
+        ConfirmedMouth.GetComponent<Image>().sprite = MouthOne.GetComponent<Image>().sprite;
+    }
+    public void SelectedMouth2()
+    {
+        selectedMouth = true;
+        ConfirmedMouth.GetComponent<Image>().sprite = MouthTwo.GetComponent<Image>().sprite;
+    }
+    public void SelectedMouth3()
+    {
+        selectedMouth = true;
+        ConfirmedMouth.GetComponent<Image>().sprite = MouthThree.GetComponent<Image>().sprite;
+    }
+    public void SelectedMouth4()
+    {
+        selectedMouth = true;
+        ConfirmedMouth.GetComponent<Image>().sprite = MouthFour.GetComponent<Image>().sprite;
+    }
+}
---
+++ Assets/Scripts/Overlay/Overlay_Shop.cs
@@ -0,0 +1,257 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay_Shop : MonoBehaviour {
+    [SerializeField]
+    GameObject BuyButton;
+    [SerializeField]
+    GameObject SellButton;
+    [SerializeField]
+    GameObject RepairButton;
+    [SerializeField]
+    GameObject RebuyButton;
+
+    [SerializeField]
+    GameObject BuyTab;
+    //[SerializeField]
+    //GameObject SellTab;
+    //[SerializeField]
+    //GameObject RepairTab;
+    //[SerializeField]
+    //GameObject RebuyTab;
+
+    [SerializeField]
+    GameObject BuyEquipmentButton;
+    [SerializeField]
+    GameObject BuyUseButton;
+    [SerializeField]
+    GameObject BuyMaterialButton;
+
+    [SerializeField]
+    GameObject BuyEquipmentTab;
+    //[SerializeField]
+    //GameObject BuyUseTab;
+    //[SerializeField]
+    //GameObject BuyMaterialTab;
+
+    [SerializeField]
+    GameObject ItemTab;
+
+    bool b_buyOpen;
+    bool b_sellOpen;
+    bool b_repairOpen;
+    bool b_rebuyOpen;
+
+    bool b_buyequipmentOpen;
+    bool b_buyuseOpen;
+    bool b_buymaterialOpen;
+
+    bool b_itemOpen;
+
+
+    public Color ButtonActiveColour = Color.red;
+    public Color ButtonInactiveColour = Color.cyan;
+
+
+    // Use this for initialization
+    void Start () {
+        b_buyOpen = true;
+        b_sellOpen = false;
+        b_repairOpen = false;
+        b_rebuyOpen = false;
+
+        BuyButton.GetComponent<Image>().color = ButtonActiveColour;
+        SellButton.GetComponent<Image>().color = ButtonInactiveColour;
+        RepairButton.GetComponent<Image>().color = ButtonInactiveColour;
+        RebuyButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        b_buyequipmentOpen = true;
+        b_buyuseOpen = false;
+        b_buymaterialOpen = false;
+
+        BuyEquipmentButton.GetComponent<Image>().color = ButtonActiveColour;
+        BuyUseButton.GetComponent<Image>().color = ButtonInactiveColour;
+        BuyMaterialButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        b_itemOpen = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        BuyTab.SetActive(b_buyOpen);
+        //SellTab.SetActive(b_sellOpen);
+        //RepairTab.SetActive(b_repairOpen);
+        //RebuyTab.SetActive(b_rebuyOpen);
+        
+        if (b_buyOpen)
+            BuyButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            BuyButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        if (b_sellOpen)
+            SellButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            SellButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        if (b_repairOpen)
+            RepairButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            RepairButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        if (b_rebuyOpen)
+            RebuyButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            RebuyButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        BuyEquipmentTab.SetActive(b_buyequipmentOpen);
+        //BuyUseTab.SetActive(b_buyuseOpen);
+        //BuyMaterialTab.SetActive(b_buymaterialOpen);
+
+        ItemTab.SetActive(b_itemOpen);
+        
+
+        if (b_buyequipmentOpen)
+            BuyEquipmentButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            BuyEquipmentButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        if (b_buyuseOpen)
+            BuyUseButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            BuyUseButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+        if (b_buymaterialOpen)
+            BuyMaterialButton.GetComponent<Image>().color = ButtonActiveColour;
+        else
+            BuyMaterialButton.GetComponent<Image>().color = ButtonInactiveColour;
+
+
+    }
+
+    //buy
+    public void Buy_Open()
+    {
+        Debug.Log("buy_Opened");
+        b_buyOpen = true;
+        b_sellOpen = false;
+        b_repairOpen = false;
+        b_rebuyOpen = false;
+        
+    }
+    public void Buy_Close()
+    {
+        Debug.Log("buy_Closed");
+        b_buyOpen = false;
+    }
+
+    //sell
+    public void Sell_Open()
+    {
+        Debug.Log("sell_Opened");
+        b_buyOpen = false;
+        b_sellOpen = true;
+        b_repairOpen = false;
+        b_rebuyOpen = false;
+
+    }
+    public void Sell_Close()
+    {
+        Debug.Log("sell_Closed");
+        b_sellOpen = false;
+    }
+
+    //repair
+    public void Repair_Open()
+    {
+        Debug.Log("repair_Opened");
+        b_buyOpen = false;
+        b_sellOpen = false;
+        b_repairOpen = true;
+        b_rebuyOpen = false;
+
+    }
+    public void Repair_Close()
+    {
+        Debug.Log("repair_Closed");
+        b_repairOpen = false;
+    }
+
+    //rebuy
+    public void Rebuy_Open()
+    {
+        Debug.Log("rebuy_Opened");
+        b_buyOpen = false;
+        b_sellOpen = false;
+        b_repairOpen = false;
+        b_rebuyOpen = true;
+
+    }
+    public void Rebuy_Close()
+    {
+        Debug.Log("rebuy_Closed");
+        b_rebuyOpen = false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    //buy equipment
+    public void Buyequipment_Open()
+    {
+        Debug.Log("buyequiment_Opened");
+        b_buyequipmentOpen = true;
+        b_buyuseOpen = false;
+        b_buymaterialOpen = false;
+
+    }
+    public void Buyequipment_Close()
+    {
+        Debug.Log("buyequiment_Closed");
+        b_buyequipmentOpen = false;
+    }
+
+    //buy use
+    public void Buyuse_Open()
+    {
+        Debug.Log("buyuse_Open");
+        b_buyequipmentOpen = false;
+        b_buyuseOpen = true;
+        b_buymaterialOpen = false;
+
+    }
+    public void Buyuse_Close()
+    {
+        Debug.Log("buyuse_Close");
+        b_buyuseOpen = false;
+    }
+
+    //buy material
+    public void Buymaterial_Open()
+    {
+        Debug.Log("buymaterial_Open");
+        b_buyequipmentOpen = false;
+        b_buyuseOpen = false;
+        b_buymaterialOpen = true;
+
+    }
+    public void Buymaterial_Close()
+    {
+        Debug.Log("buymaterial_Close");
+        b_buymaterialOpen = false;
+    }
+
+    //buy item1
+    public void Buyitem_Open()
+    {
+        Debug.Log("buyitem1_Open");
+        b_itemOpen = true;
+        Debug.Log(b_itemOpen);
+    }
+    public void Buyitem_Close()
+    {
+        Debug.Log("buyitem1_close");
+        b_itemOpen = false;
+    }
+}
